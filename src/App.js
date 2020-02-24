546620f7b31c6aff323f9f73602bf2e9d0706c05
CN24 <codenation@CN24s-MacBook-Pro.local>
2020-02-24 13:21:18 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    list: [],
+    currentInput: " "
+  }
+
+  addHandler = e => {
+    this.setState({ currentInput: e.target.value })
+    
+  }
+
+  submit = () => {
+    let storeInput = this.state.list
+    storeInput.push(this.state.currentInput)
+    this.setState({input: storeInput, currentInput: ""})
+    console.log("Hi o/")
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <div className="addressBody">
+          <button onClick={this.submit}>+</button>
+        <input type="text" value={this.state.currentInput} onChange={this.addHandler}></input>
+        {this.state.list.map((savedInput, index) => {
+          return <p key={index}>{savedInput}</p>
+        })}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
