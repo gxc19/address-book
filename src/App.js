a6fb96b0eda46fff75d601815ed2cf5497080226
Ga Ming Chan <gmchan19@gmail.com>
2020-03-03 17:26:43 +0000

    working address-book

--- src/App.js
+++ src/App.js
@@ -14,9 +14,19 @@ class App extends Component {
 
   submit = () => {
     let storeInput = this.state.list
+
+    if(this.state.currentInput === ""){
+      return alert("Please Type In The Space Below")
+    }
     storeInput.push(this.state.currentInput)
     this.setState({input: storeInput, currentInput: ""})
     console.log("Hi o/")
+  }
+
+  removeHandle = index => {
+    let storeList = this.state.list
+    storeList.splice(index, 1)
+    this.setState({ list: storeList})
   }
 
   render () {
@@ -26,7 +36,7 @@ class App extends Component {
           <button onClick={this.submit}>+</button>
         <input type="text" value={this.state.currentInput} onChange={this.addHandler}></input>
         {this.state.list.map((savedInput, index) => {
-          return <p key={index}>{savedInput}</p>
+          return <p key={index}>{savedInput} onClick={() => this.removeHandle(index)}</p>
         })}
         </div>
       </div>
