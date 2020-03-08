4f04501e33fc7bc18fa81a9f8cd8dcbbc0efc8bb
Ga-Ming Chan <Ga-Ming@Laras-iMac.local>
2020-03-08 21:12:30 +0000

    Updates

---
+++ precache-manifest.52fa99b8fb01231aee97bf40aacee13f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff8a338e350ef021274d3ebac34f191",
+    "url": "/address-book/index.html"
+  },
+  {
+    "revision": "1c06f66aa3a4464efb4e",
+    "url": "/address-book/static/css/main.8e4f7e68.chunk.css"
+  },
+  {
+    "revision": "eb344fbf45586d97d28b",
+    "url": "/address-book/static/js/2.b6bf8255.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/address-book/static/js/2.b6bf8255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c06f66aa3a4464efb4e",
+    "url": "/address-book/static/js/main.900a15b7.chunk.js"
+  },
+  {
+    "revision": "01281dc8e04fdff61e51",
+    "url": "/address-book/static/js/runtime-main.9cf7cf23.js"
+  }
+]);
